83a6391052b6f996b02e863b2e8a8f30ee63169b
Gaufre <191967922+gaufre2@users.noreply.github.com>
2025-10-09 13:32:03 +0200

    feat: create media class

---
+++ src/media.spec.ts
@@ -0,0 +1,26 @@
+import type { Torrent } from "@ctrl/qbittorrent";
+import { describe, test, expect } from "bun:test";
+import { readFileSync } from "fs";
+import { Torrents } from "./torrent";
+import { WatchedMedia, WatchedMedias } from "./media";
+
+describe("Torrents from qbittorrent", () => {
+  const fakeTorrents: Torrent[] = JSON.parse(
+    readFileSync("./test/get-torrent-list.mock.json", "utf-8")
+  );
+
+  describe("Parse torrents", () => {
+    test("Get the first torrent", () => {
+      const torrents = new Torrents(fakeTorrents);
+      const watchedMedia = new WatchedMedia(torrents.info[0]);
+      expect(watchedMedia.torrent.name).toBe(fakeTorrents[0].name);
+    });
+
+    test("Get all torrents", () => {
+      const torrents = new Torrents(fakeTorrents);
+      const watched = new WatchedMedias();
+      watched.addMediaFromInfoTorrents(torrents.info);
+      expect(watched.medias.length).toBe(fakeTorrents.length);
+    });
+  });
+});
---
+++ src/media.ts
@@ -0,0 +1,25 @@
+import type { InfoTorrent } from "./torrent";
+
+export class WatchedMedia {
+  public readonly torrent: InfoTorrent;
+
+  constructor(torrent: InfoTorrent) {
+    this.torrent = torrent;
+  }
+}
+
+export class WatchedMedias {
+  public readonly medias: WatchedMedia[];
+
+  constructor() {
+    this.medias = [];
+  }
+
+  addMedia(media: WatchedMedia): void {
+    this.medias.push(media);
+  }
+
+  addMediaFromInfoTorrents(torrents: InfoTorrent[]): void {
+    torrents.forEach((torrent) => this.addMedia(new WatchedMedia(torrent)));
+  }
+}
