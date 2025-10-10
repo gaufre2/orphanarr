cbabc76242ae0d3bf3782b2411853c082fe23c33
Gaufre <191967922+gaufre2@users.noreply.github.com>
2025-10-10 10:24:39 +0000

    test: refractor file tests to be closer to the needs
    + fix error catching

--- src/file.spec.ts
+++ src/file.spec.ts
@@ -5,93 +5,246 @@ import { mkdirSync, rmSync, writeFileSync } from "node:fs";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
 
-let testDir: string;
-let targetDir: string;
-let linkDir: string;
+let tempDir: string;
+let fakeTorrentMovieDir: string;
+let fakeTorrentSeriesDir: string;
+let fakeRadarrDir: string;
+let fakeSonarrDir: string;
 
 beforeAll(() => {
   // Create unique test directory for each test
-  testDir = join(
+  tempDir = join(
     tmpdir(),
     `test-${Date.now()}-${Math.random().toString(36).substring(7)}`
   );
 
-  targetDir = join(testDir, "source");
-  linkDir = join(testDir, "link");
+  fakeTorrentMovieDir = join(tempDir, "completed/movies");
+  fakeTorrentSeriesDir = join(tempDir, "completed/series");
+  fakeRadarrDir = join(tempDir, "media/movies");
+  fakeSonarrDir = join(tempDir, "media/series");
 
-  mkdirSync(targetDir, { recursive: true });
-  mkdirSync(linkDir, { recursive: true });
+  mkdirsSyncRecursively([
+    fakeTorrentMovieDir,
+    fakeTorrentMovieDir,
+    fakeRadarrDir,
+    fakeSonarrDir,
+  ]);
 
-  // Create  target files
-  writeFileSync(join(targetDir, "file1"), "1");
-  writeFileSync(join(targetDir, "file2"), "12");
-  writeFileSync(join(targetDir, "file3"), "123");
-  writeFileSync(join(targetDir, "file4"), "1234");
+  // Create torrent movie file
+  mkdirsSyncRecursively([
+    join(fakeTorrentMovieDir, "MovieWithLinksInAFolder"),
+    join(fakeTorrentMovieDir, "MoviesNestedInFolders/Nested"),
+  ]);
+  writeFilesSyncWithRandomData([
+    join(fakeTorrentMovieDir, "MovieWithLinks.mkv"),
+    join(fakeTorrentMovieDir, "MovieWithLinksInAFolder/Movie.mkv"),
+    join(fakeTorrentMovieDir, "MovieWithoutLinksButUnwatchExtension.iso"),
+    join(fakeTorrentMovieDir, "MoviesNestedInFolders/MovieAtRoot1.mkv"),
+    join(fakeTorrentMovieDir, "MoviesNestedInFolders/MovieAtRoot2.mkv"),
+    join(
+      fakeTorrentMovieDir,
+      "MoviesNestedInFolders/Nested/MovieInNestedFolder.mkv"
+    ),
+  ]);
 
-  // Create hard linked copy
-  execSync(`ln "${join(targetDir, "file1")}" "${join(linkDir, "file1a")}"`);
-  execSync(`ln "${join(targetDir, "file1")}" "${join(linkDir, "file1b")}"`);
+  // Create torrent series file
+  mkdirsSyncRecursively([
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 01"),
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 02"),
+    join(fakeTorrentSeriesDir, "SeriesWithoutLinksToDelete/Season 01"),
+  ]);
+  writeFilesSyncWithRandomData([
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 01/Episode 01.mkv"),
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 01/Episode 02.mkv"),
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 01/Episode 03.mkv"),
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 02/Episode 01.mkv"),
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 02/Episode 02.mkv"),
+    join(fakeTorrentSeriesDir, "SeriesWithLinks/Season 02/Episode 03.mkv"),
+    join(
+      fakeTorrentSeriesDir,
+      "SeriesWithoutLinksToDelete/Season 01/Episode 01.mkv"
+    ),
+    join(
+      fakeTorrentSeriesDir,
+      "SeriesWithoutLinksToDelete/Season 01/Episode 02.mkv"
+    ),
+  ]);
 
-  execSync(`ln "${join(targetDir, "file3")}" "${join(linkDir, "file3a")}"`);
-  execSync(`ln "${join(targetDir, "file3")}" "${join(linkDir, "file3b")}"`);
-  execSync(`ln "${join(targetDir, "file3")}" "${join(linkDir, "file3c")}"`);
-  execSync(`ln "${join(targetDir, "file3")}" "${join(linkDir, "file3d")}"`);
+  // Create radarr dirs
+  mkdirsSyncRecursively([
+    join(fakeRadarrDir, "MovieWithLinks"),
+    join(fakeRadarrDir, "MovieWithLinksInAFolder"),
+    join(fakeRadarrDir, "MovieWithoutLinksButUnwatchExtension"),
+  ]);
 
-  execSync(`ln "${join(targetDir, "file4")}" "${join(linkDir, "file4a")}"`);
-  execSync(`ln "${join(targetDir, "file4")}" "${join(linkDir, "file4b")}"`);
-  execSync(`ln "${join(targetDir, "file4")}" "${join(linkDir, "file4c")}"`);
+  // Create radarr hard linked copy
+  generateHardLinkCopies([
+    {
+      target: join(fakeTorrentMovieDir, "MovieWithLinks.mkv"),
+      link: join(fakeRadarrDir, "MovieWithLinks/Movie.Link1.mkv"),
+    },
+    {
+      target: join(fakeTorrentMovieDir, "MovieWithLinks.mkv"),
+      link: join(fakeRadarrDir, "MovieWithLinks/Movie.Link2.mkv"),
+    },
+    {
+      target: join(fakeTorrentMovieDir, "MovieWithLinksInAFolder/Movie.mkv"),
+      link: join(fakeRadarrDir, "MovieWithLinksInAFolder/Movie.Link1.mkv"),
+    },
+    {
+      target: join(
+        fakeTorrentMovieDir,
+        "MovieWithoutLinksButUnwatchExtension.iso"
+      ),
+      link: join(
+        fakeRadarrDir,
+        "MovieWithoutLinksButUnwatchExtension/Movie.Link1.iso"
+      ),
+    },
+  ]);
 
-  // Remove file 4
-  rmSync(join(targetDir, "file4"));
+  // Create a radarr file witout hard link
+  writeFilesSyncWithRandomData([
+    join(fakeRadarrDir, "MovieWithoutHardLink.mkv"),
+  ]);
 
-  /*
-  Links mapping:
-    file1
-    ├── file1a
-    └── file1b
-    file2
-    └── (no links)
-    file3
-    ├── file3a
-    ├── file3b
-    ├── file3c
-    └── file3d
-    (deleted)
-    ├── file4a
-    ├── file4b
-    └── file4c
-  */
+  // create sonarr dirs
+  mkdirsSyncRecursively([
+    join(fakeSonarrDir, "SeriesWithLinks/Season 01"),
+    join(fakeSonarrDir, "SeriesWithLinks/Season 02"),
+  ]);
+
+  // create sonarr hard linked copy
+  generateHardLinkCopies([
+    {
+      target: join(
+        fakeTorrentSeriesDir,
+        "SeriesWithLinks/Season 01/Episode 01.mkv"
+      ),
+      link: join(
+        fakeSonarrDir,
+        "SeriesWithLinks/Season 01/Episode 01.Link1.mkv"
+      ),
+    },
+    {
+      target: join(
+        fakeTorrentSeriesDir,
+        "SeriesWithLinks/Season 01/Episode 02.mkv"
+      ),
+      link: join(
+        fakeSonarrDir,
+        "SeriesWithLinks/Season 01/Episode 02.Link1.mkv"
+      ),
+    },
+    {
+      target: join(
+        fakeTorrentSeriesDir,
+        "SeriesWithLinks/Season 01/Episode 03.mkv"
+      ),
+      link: join(
+        fakeSonarrDir,
+        "SeriesWithLinks/Season 01/Episode 03.Link1.mkv"
+      ),
+    },
+    {
+      target: join(
+        fakeTorrentSeriesDir,
+        "SeriesWithLinks/Season 02/Episode 01.mkv"
+      ),
+      link: join(
+        fakeSonarrDir,
+        "SeriesWithLinks/Season 02/Episode 01.Link1.mkv"
+      ),
+    },
+    {
+      target: join(
+        fakeTorrentSeriesDir,
+        "SeriesWithLinks/Season 02/Episode 02.mkv"
+      ),
+      link: join(
+        fakeSonarrDir,
+        "SeriesWithLinks/Season 02/Episode 02.Link1.mkv"
+      ),
+    },
+    {
+      target: join(
+        fakeTorrentSeriesDir,
+        "SeriesWithLinks/Season 02/Episode 03.mkv"
+      ),
+      link: join(
+        fakeSonarrDir,
+        "SeriesWithLinks/Season 02/Episode 03.Link1.mkv"
+      ),
+    },
+  ]);
+
+  function writeFilesSyncWithRandomData(filePaths: string[]): void {
+    filePaths.forEach((filePath) =>
+      writeFileSync(filePath, generateRandomData(10))
+    );
+  }
+
+  function generateRandomData(sizeBytes: number): Uint8Array {
+    return crypto.getRandomValues(new Uint8Array(sizeBytes));
+  }
+
+  function mkdirsSyncRecursively(dirPaths: string[]): void {
+    dirPaths.forEach((dirPath) => mkdirSync(dirPath, { recursive: true }));
+  }
+
+  interface HardLinkCopies {
+    target: string;
+    link: string;
+  }
+
+  function generateHardLinkCopies(hardLinks: HardLinkCopies[]): void {
+    hardLinks.forEach((hardLink) => generateHardLinkCopy(hardLink));
+  }
+
+  function generateHardLinkCopy(hardLink: HardLinkCopies): void {
+    execSync(`ln "${hardLink.target}" "${hardLink.link}"`);
+  }
 });
 
 afterAll(() => {
-  rmSync(testDir, { recursive: true, force: true });
+  rmSync(tempDir, { recursive: true, force: true });
 });
 
 describe("Hard linking check", () => {
   test("2 files are hard linked (from path)", async () => {
-    const torrentFile = await FileExtended.fromPath(join(targetDir, "file1"));
-    expect(await torrentFile.isHardLinkedWith(join(linkDir, "file1a"))).toBe(
-      true
+    const torrentFile = await FileExtended.fromPath(
+      join(fakeTorrentMovieDir, "MovieWithLinks.mkv")
     );
-  });
-
-  test("2 files are not hard linked (from BunFile)", async () => {
-    const torrentFile = await FileExtended.fromPath(join(targetDir, "file1"));
-    const comparaisonBunFile = await Bun.file(join(linkDir, "file1a"));
-    expect(await torrentFile.isHardLinkedWith(comparaisonBunFile)).toBe(true);
+    expect(
+      await torrentFile.isHardLinkedWith(
+        join(fakeRadarrDir, "MovieWithLinks/Movie.Link1.mkv")
+      )
+    ).toBe(true);
   });
 
   test("2 files are not hard linked", async () => {
-    const torrentFile = await FileExtended.fromPath(join(targetDir, "file1"));
-    expect(await torrentFile.isHardLinkedWith(join(linkDir, "file3a"))).toBe(
-      false
+    const torrentFile = await FileExtended.fromPath(
+      join(fakeTorrentMovieDir, "MovieWithLinks.mkv")
     );
+    expect(
+      await torrentFile.isHardLinkedWith(
+        join(fakeRadarrDir, "MovieWithoutHardLink.mkv")
+      )
+    ).toBe(false);
   });
 
   test("1 file is invalid", async () => {
-    const torrentFile = await FileExtended.fromPath(join(targetDir, "file1"));
-    expect(
-      torrentFile.isHardLinkedWith(join(linkDir, "invalid"))
-    ).rejects.toThrow();
+    const torrentFile = await FileExtended.fromPath(
+      join(fakeTorrentMovieDir, "MovieWithLinks.mkv")
+    );
+    expect.assertions(1);
+    try {
+      await torrentFile.isHardLinkedWith(join(fakeRadarrDir, "invalid"));
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+    }
+  });
+});
+
   });
 });
